7ce93c62cdd86719f52b8370583bbf22a98d3e47
Henry Mao <admin@calclavia.com>
2017-04-07 15:08:47 -0700

    Check note-axis

--- models.py
+++ models.py
@@ -231,8 +231,79 @@ def build_model(time_steps=TIME_STEPS, input_dropout=0.2, dropout=0.5):
     out = time_axis_model([notes_in, beat_in, style_in])
 
     # Apply note-axis model
-    note_axis_model = note_axis(time_steps, input_dropout, dropout)
-    out = note_axis_model([out, chosen_in, style_in])
+    # note_axis_model = note_axis(time_steps, input_dropout, dropout)
+    # out = note_axis_model([out, chosen_in, style_in])
+
+    # Style linear projection
+    style_distributed = TimeDistributed(Dense(STYLE_UNITS))(style_in)
+    
+    # Shift target one note to the left. []
+    shift_chosen = Lambda(lambda x: tf.pad(x[:, :, :-1], [[0, 0], [0, 0], [1, 0]]))(chosen_in)
+    shift_chosen = Dropout(input_dropout)(shift_chosen)
+    shift_chosen = Lambda(lambda x: tf.expand_dims(x, -1))(shift_chosen)
+
+    # Define shared layers
+    note_axis_conv_tanh = [Conv1D(units, 2, dilation_rate=2 ** l, padding='causal', name='note_axis_conv_tanh_' + str(l)) for l, units in enumerate(NOTE_AXIS_UNITS)]
+    note_axis_conv_sig = [Conv1D(units, 2, dilation_rate=2 ** l, padding='causal', name='note_axis_conv_sig_' + str(l)) for l, units in enumerate(NOTE_AXIS_UNITS)]
+
+    note_axis_conv_skip = [Conv1D(units, 1, padding='same', name='note_axis_conv_skip_' + str(l)) for l, units in enumerate(NOTE_AXIS_UNITS)]
+
+    note_axis_conv_final = [Conv1D(units, 1, padding='same', name='note_axis_conv_final_' + str(l)) for l, units in enumerate(FINAL_UNITS)]
+
+    prediction_layer = Dense(1, activation='sigmoid')
+    note_axis_outs = []
+
+    # [batch, time, notes, 1]
+    shift_chosen = Reshape((time_steps, NUM_NOTES, -1))(shift_chosen)
+    # [batch, time, notes, features + 1]
+    note_axis_input = Concatenate(axis=3)([out, shift_chosen])
+
+    for t in range(time_steps):
+        # [batch, notes, features + 1]
+        note_axis_out = Lambda(lambda x: x[:, t, :, :], name='time_' + str(t))(note_axis_input)
+        style_sliced = RepeatVector(NUM_NOTES)(Lambda(lambda x: x[:, t, :], name='style_tanh_' + str(t))(style_distributed))
+
+        """
+        first_layer_out = note_axis_out = Dropout(dropout)(note_axis_rnn_1(note_axis_out))
+        note_axis_out = Dropout(dropout)(note_axis_rnn_2(note_axis_out))
+        # Residual connection
+        note_axis_out = Add()([first_layer_out, note_axis_out])
+        """
+        skips = []
+        # Create large enough dilation to cover all notes
+        for l, units in enumerate(NOTE_AXIS_UNITS):
+            prev_out = note_axis_out
+
+            # Gated activation unit.
+            tanh_out = Activation('tanh')(Add()([note_axis_conv_tanh[l](note_axis_out), style_sliced]))
+            sig_out = Activation('sigmoid')(Add()([note_axis_conv_sig[l](note_axis_out), style_sliced]))
+            # z = tanh(Wx + Vh) x sigmoid(Wx + Vh) from Wavenet
+            note_axis_out = Multiply()([tanh_out, sig_out])
+            note_axis_out = Dropout(dropout)(note_axis_out)
+
+            # Res conv connection
+            res_out = note_axis_out
+
+            # Skip connection
+            skips.append(note_axis_conv_skip[l](note_axis_out))
+
+            # Residual connection
+            if l > 0:
+                note_axis_out = Add()([res_out, prev_out])
+
+        # Merge all skip connections. Improves convergence and output.
+        note_axis_out = Add()(skips)
+
+        for l, units in enumerate(FINAL_UNITS):
+            note_axis_out = Activation('relu')(note_axis_out)
+            note_axis_out = note_axis_conv_final[l](note_axis_out)
+            note_axis_out = Dropout(dropout)(note_axis_out)
+
+        # Apply prediction layer
+        note_axis_out = prediction_layer(note_axis_out)
+        note_axis_out = Reshape((NUM_NOTES,))(note_axis_out)
+        note_axis_outs.append(note_axis_out)
+    out = Lambda(lambda x: tf.stack(x, axis=1))(note_axis_outs)
 
     model = Model([notes_in, chosen_in, beat_in, style_in], out)
     model.compile(optimizer='adam', loss='binary_crossentropy')
