03cb3881fe9a3ae1c76e6462d513ddd8c51cbd2e
Taylor Shin <taylorshin921@gmail.com>
2017-05-09 13:32:47 -0700

    remove computing merged notes

--- midi_util.py
+++ midi_util.py
@@ -104,34 +104,26 @@ def midi_decode(pattern,
         step = pattern.resolution // NOTES_PER_BEAT
 
     # Extract all tracks at highest resolution
-    merged_notes = None
     merged_replay = None
     merged_volume = None
 
     for track in pattern:
         # The downsampled sequences
-        play_sequence = []
         replay_sequence = []
         volume_sequence = []
 
         # Raw sequences
-        play_buffer = [np.zeros((classes,))]
         replay_buffer = [np.zeros((classes,))]
         volume_buffer = [np.zeros((classes,))]
 
         for i, event in enumerate(track):
             # Duplicate the last note pattern to wait for next event
             for _ in range(event.tick):
-                play_buffer.append(np.copy(play_buffer[-1]))
                 replay_buffer.append(np.zeros(classes))
                 volume_buffer.append(np.copy(volume_buffer[-1]))
 
                 # Buffer & downscale sequence
-                if len(play_buffer) > step:
-                    # Determine based on rounded sum
-                    notes_sum = np.round(np.sum(play_buffer[:-1], axis=0) / step)
-                    play_sequence.append(notes_sum)
-
+                if len(volume_buffer) > step:
                     # Take the min
                     replay_any = np.minimum(np.sum(replay_buffer[:-1], axis=0), 1)
                     replay_sequence.append(replay_any)
@@ -141,7 +133,6 @@ def midi_decode(pattern,
                     volume_sequence.append(volume_sum)
 
                     # Keep the last one (discard things in the middle)
-                    play_buffer = play_buffer[-1:]
                     replay_buffer = replay_buffer[-1:]
                     volume_buffer = volume_buffer[-1:]
 
@@ -151,44 +142,35 @@ def midi_decode(pattern,
             # Modify the last note pattern
             if isinstance(event, midi.NoteOnEvent):
                 pitch, velocity = event.data
-                play_buffer[-1][pitch] = 1 if velocity > 0 else 0
                 volume_buffer[-1][pitch] = velocity / MAX_VELOCITY
 
                 # Check for replay_buffer, which is true if the current note was previously played and needs to be replayed
-                if len(play_buffer) > 1 and play_buffer[-2][pitch] > 0 and play_buffer[-1][pitch] > 0:
+                if len(volume_buffer) > 1 and volume_buffer[-2][pitch] > 0 and volume_buffer[-1][pitch] > 0:
                     replay_buffer[-1][pitch] = 1
                     # Override current volume with previous volume
-                    play_buffer[-1][pitch] = play_buffer[-2][pitch]
+                    volume_buffer[-1][pitch] = volume_buffer[-2][pitch]
 
             if isinstance(event, midi.NoteOffEvent):
                 pitch, velocity = event.data
-                play_buffer[-1][pitch] = 0
                 volume_buffer[-1][pitch] = 0
 
         # Add the remaining
-        play_sequence.append(play_buffer[0])
         replay_any = np.minimum(np.sum(replay_buffer, axis=0), 1)
         replay_sequence.append(replay_any)
         volume_sequence.append(volume_buffer[0])
 
-        play_sequence = np.array(play_sequence)
         replay_sequence = np.array(replay_sequence)
         volume_sequence = np.array(volume_sequence)
-        assert len(play_sequence) == len(replay_sequence) and len(play_sequence) == len(volume_sequence)
+        assert len(volume_sequence) == len(replay_sequence)
 
-        if merged_notes is None:
-            merged_notes = play_sequence
+        if merged_volume is None:
             merged_replay = replay_sequence
             merged_volume = volume_sequence
         else:
             # Merge into a single track, padding with zeros of needed
-            if len(play_sequence) > len(merged_notes):
+            if len(volume_sequence) > len(merged_volume):
                 # Swap variables such that merged_notes is always at least
                 # as large as play_sequence
-                tmp = play_sequence
-                play_sequence = merged_notes
-                merged_notes = tmp
-
                 tmp = replay_sequence
                 replay_sequence = merged_replay
                 merged_replay = tmp
@@ -197,14 +179,13 @@ def midi_decode(pattern,
                 volume_sequence = merged_volume
                 merged_volume = tmp
 
-            assert len(merged_notes) >= len(play_sequence)
+            assert len(merged_volume) >= len(volume_sequence)
 
-            diff = len(merged_notes) - len(play_sequence)
-            merged_notes += np.pad(play_sequence, ((0, diff), (0, 0)), 'constant')
+            diff = len(merged_volume) - len(volume_sequence)
             merged_replay += np.pad(replay_sequence, ((0, diff), (0, 0)), 'constant')
             merged_volume += np.pad(volume_sequence, ((0, diff), (0, 0)), 'constant')
 
-    merged = np.stack([merged_notes, merged_replay, merged_volume], axis=2)
+    merged = np.stack([np.ceil(merged_volume), merged_replay, merged_volume], axis=2)
     # Prevent stacking duplicate notes to exceed one.
     merged = np.minimum(merged, 1)
     return merged
@@ -230,6 +211,7 @@ def load_midi(fname):
 
 if __name__ == '__main__':
     # Test
-    p = midi.read_midifile("out/test_in.mid")
+    # p = midi.read_midifile("out/test_in.mid")
+    p = midi.read_midifile("data/baroque/bach/0864_01.mid")
     p = midi_encode(midi_decode(p))
     midi.write_midifile("out/test_out.mid", p)
